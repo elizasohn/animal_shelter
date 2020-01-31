e89bd2d45a7f283d9b3508b901c0cbb5b6bba3e2
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-31 14:36:43 -0800

    add response module

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,6 @@
 class Animal < ApplicationRecord
+  validates :name, presence: true
+  validates :kind, presence: true
+  validates :breed, presence: true
+  validates :age, presence: true
 end
---
+++ spec/requests/post_animal_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe 'POST /animals', :type => :request do
+  context '(valid request)' do
+    before do
+      post '/animals', params: { :name => 'fuzzy', :kind => "cat", :breed => "maine coon", :age => 3 }
+    end
+
+    it 'returns the name' do
+      expect(JSON.parse(response.body)['name']).to eq('fuzzy')
+    end
+
+    it 'returns the kind content' do
+      expect(JSON.parse(response.body)['kind']).to eq('cat')
+    end
+
+    it 'returns the breed' do
+      expect(JSON.parse(response.body)['breed']).to eq('maine coon')
+    end
+
+    it 'returns the age' do
+      expect(JSON.parse(response.body)['age']).to eq(3)
+    end
+
+
+    it 'returns a created status' do
+      expect(response).to have_http_status(:created)
+    end
+    # end
+    #
+
+    it 'returns a detailed success message' do
+      expect(JSON.parse(response.body)['id']).to be_an_instance_of(Integer)
+    end
+
+    it 'returns status code 201' do
+      expect(response).to have_http_status 201
+    end
+  end
+end
+
+#   context '(bad request)' do
+#     before do
+#       post '/animals', params: { :name => '', :kind => '', :breed => '', :age => '' }
+#     end
+#
+#     it 'returns a detailed error message' do
+#       expect(JSON.parse(response.body)['message']).to eq 'Validation failed: Name can\'t be blank, Kind can\'t be blank, Breed can\'t be blank, Age can\'t be blank'
+#     end
+#
+#     it 'returns status code 422 if validations fail' do
+#       expect(response).to have_http_status 422
+#     end
+#   end
+# end
