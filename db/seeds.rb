fedd5840b2331b4df0d1825a36c3968234f722ee
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-31 11:24:58 -0800

    add seeds for cats and dogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+    def self.begin(count = 20)
+        puts ''
+        puts 'Seeds:'
+        e
+        generate_animals(count)
+    end
+
+    private
+    def self.e
+        Review.destroy_all
+    end
+
+    def self.generate_animals(count)
+        count.times do |i|
+            animal = Animal.create!(
+                name: Faker::Creature::Cat.name,
+                breed: Faker::Creature::Cat.breed,
+                age: Faker::Number.between(from: 1, to: 14)
+            )
+        end
+        count.times do |i|
+            animal = Animal.create!(
+                name: Faker::Creature::Dog.name,
+                breed: Faker::Creature::Dog.breed,
+                age: Faker::Number.between(from: 1, to: 14)
+            )
+        end
+        puts "  Created #{Review.count} cats and #{Review.count} dogs."
+    end
+end
+
+Seed.begin
